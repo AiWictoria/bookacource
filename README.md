d27b734472083720e110efe2603f474fc80d10cd
AiWictoria <Viktoria.pakhrova@outlook.com>
2024-10-31 21:48:35 +0100

    ran all files through validator and corrected some warnings and error on select element + deleted some extra tabs in code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bookacource
-Skapa en webbsida för ett fiktivt företag som erbjuder kurser inom ett expertområde
+# Skapa en webbsida för ett fiktivt företag som erbjuder kurser inom ett expertområde
--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-
-
 const hamMenu = document.querySelector(".hammenu");
-
 const offScreenMenu = document.querySelector(".offscreenmenu");
-
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
